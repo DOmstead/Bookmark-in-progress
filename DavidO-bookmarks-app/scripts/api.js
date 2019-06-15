4f9c01ed4c34e754754fc1b358d63c21d8a434c5
DOmstead <49734565+DOmstead@users.noreply.github.com>
2019-06-15 15:32:54 -0600

    added full circle functionality

---
+++ DavidO-bookmarks-app/scripts/api.js
@@ -0,0 +1,55 @@
+/* eslint-disable indent */
+'use strict';
+
+const api = (function(){
+  let name = 'davidO';
+  let baseURL = `https://thinkful-list-api.herokuapp.com/${name}/bookmarks`;
+
+  const getBookmarks = function(){
+    return fetch(baseURL)
+      .then( res => {
+        return res.json();
+      } )
+      .then(data => {
+        return data;
+      });
+  };
+
+  const createBookmark = function(title,url,desc = ' ',rating = null){
+    console.log('Create Bookmark Ran');
+    // const newBookmark = JSON.stringify({ title,url,desc, });
+    const newBookmark = `{"title": "${title}", "url":"${url}", "desc":"${desc}", "rating": ${rating}}`;
+    console.log(newBookmark);
+    return fetch(baseURL,{
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: newBookmark
+    })
+      .then( res => {
+        return res.json();
+      } )
+      .then(data => {
+        return data;
+      });
+  };
+
+  const deleteBookmarks = function(id){
+    return fetch(`${baseURL}/${id}`,{
+      method:'DELETE'
+    })
+      .then( res => {
+        return res.json();
+      } );
+    //   .then(data => {
+    //     return data;
+    //   });
+  };
+
+  return {
+    deleteBookmarks,  
+    getBookmarks,
+    createBookmark
+  };
+}());
